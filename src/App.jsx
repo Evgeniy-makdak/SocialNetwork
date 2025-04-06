4672c3282a16aa9596add4835fa32a1716767fa5
Evgeniy-makdak <makdak23@mail.ru>
2025-04-06 12:44:51 +0300

    1.3.0 Вынес из App в отдельные компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,14 @@
-import logo from "./assets/logo.png";
 import "./App.css";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="app-header">
-        <img src={logo} alt="logo" />
-      </header>
-      <nav className="app-nav">
-        <div>
-          <a href="#">Profile</a>
-        </div>
-        <div>
-          <a href="#">Messages</a>
-        </div>
-        <div>
-          <a href="#">News</a>
-        </div>
-        <div>
-          <a href="#">Music</a>
-        </div>
-        <div>
-          <a href="#">Settings</a>
-        </div>
-      </nav>
-      <div className="app-content">
-        <img
-          src="https://klike.net/uploads/posts/2019-11/1574605225_2.jpg"
-          alt=""
-        />
-        <div>avatar</div>
-        <div>my_posts</div>
-        <div>new_post</div>
-        <div>post1</div>
-        <div>post2</div>
-      </div>
+      <Header />
+      <Nav />
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,15 @@
+export default function Content() {
+    return (
+        <div className="app-content">
+            <img
+                src="https://klike.net/uploads/posts/2019-11/1574605225_2.jpg"
+                alt=""
+            />
+            <div>avatar</div>
+            <div>my_posts</div>
+            <div>new_post</div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+    );
+}
