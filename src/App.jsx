23afd22e2b035e963e4a15f5be8d7cc45d900293
Evgeniy-makdak <makdak23@mail.ru>
2025-04-06 12:29:42 +0300

    1.2.0 Выделение смысловых блоков background-color для лучшей визуализации

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,11 @@ function App() {
           src="https://klike.net/uploads/posts/2019-11/1574605225_2.jpg"
           alt=""
         />
+        <div>avatar</div>
+        <div>my_posts</div>
+        <div>new_post</div>
+        <div>post1</div>
+        <div>post2</div>
       </div>
     </div>
   );
